ca394ee50df349e70bdb0a478f0de2fbe67ebb92
zainurroziqin <75110898+zainurroziqin@users.noreply.github.com>
2021-04-02 15:26:31 +0700

    Tugas Minggu Keempat

---
+++ TugasPerulanganB2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasperulangan;
+
+/**
+ *
+ * @author zia
+ */
+public class TugasPerulanganB2 {
+    public static void main(String[] args) {
+        
+        int angka = 1, i = 0;
+        do{
+            i++;
+            System.out.println(angka);
+            angka = i*2;
+            
+        }while(angka<=100);
+    }
+    
+}
